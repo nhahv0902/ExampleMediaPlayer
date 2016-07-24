773895e8516a93a4b9c37964447eb62e23683012
nha.hoang <nha.hoang@nccsoft.vn>
2016-07-24 08:07:46 +0700

    fires

---
+++ app/src/main/java/com/nhahv/mediaplayer/media/MeidaPlayerManager.java
@@ -0,0 +1,11 @@
+package com.nhahv.mediaplayer.media;
+
+/**
+ * Created by Nhahv on 7/24/2016.
+ * <></>
+ */
+public class MeidaPlayerManager {
+
+
+
+}
---
+++ app/src/main/java/com/nhahv/mediaplayer/models/MediaSong.java
@@ -0,0 +1,9 @@
+package com.nhahv.mediaplayer.models;
+
+/**
+ * Created by Nhahv on 7/24/2016.
+ * <></>
+ */
+public class MediaSong {
+
+}
